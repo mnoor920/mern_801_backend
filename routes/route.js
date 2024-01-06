6f7453b066184cc5db44b1aab4719d65fbc5b4e3
mnoor920 <mnoor92081@hotmail.com>
2024-01-06 22:34:11 +0500

    app updated

--- routes/route.js
+++ routes/route.js
@@ -7,6 +7,9 @@ import { CreateBlogPost } from '../controller/blog-post-controller.js';
 const router = express.Router();
 
 
+router.post('/', (req, res) => {
+    res.send("App Config Succefully")
+})
 router.post('/create_user', SignUp)
 router.post('/create_blog', CreateBlogPost)
 
